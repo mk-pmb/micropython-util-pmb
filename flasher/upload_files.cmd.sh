3e05447b5d3b3afa66e528ed963467385f751a53
mk-pmb <mk-pmb@users.noreply.github.com>
2020-08-22 00:09:44 +0200

    File uploader: Support LUA for original NodeMCU

--- flasher/upload_files.cmd.sh
+++ flasher/upload_files.cmd.sh
@@ -3,6 +3,7 @@
 
 
 function mpfcmd_upload_files () {
+  local REPL_LANG="${MPF_REPL_LANG:-py}"
   local ARG= DEST_DIR='/' DEST_FN=
   while [ "$#" -ge 1 ]; do
     ARG="$1"; shift
@@ -31,17 +32,35 @@ function mpfcmd_upload_files__up_one () {
       DEST_PY="unb64('$(echo -n "$DEST_ABS" | base64 --wrap=0)')";;
   esac
 
-  local MP_CODE="
-    from ubinascii import a2b_base64 as unb64
-    fh = open($DEST_PY, 'w')
-    fh.write(unb64('$DATA_B64'))
-    fh.flush()
-    fh.close()
-    "
-  MP_CODE="$(<<<"$MP_CODE" sed -re 's~^\s+~~;/^$/d')"
-  MP_CODE="${MP_CODE//$'\n'/; }"
+  local MP_CODE=
+  case "$REPL_LANG" in
+    py )
+      MP_CODE="
+        from ubinascii import a2b_base64 as unb64;
+        fh = open($DEST_PY, 'w');
+        fh.write(unb64('$DATA_B64'));
+        fh.flush();
+        fh.close();
+        ";;
+
+    lua )
+      MP_CODE="
+        local unb64 = encoder.fromBase64;
+        file.putcontents($DEST_PY, unb64('$DATA_B64'));
+        "
+      ;;
+
+    * )
+      echo "E: unsupported REPL language: '$MPF_REPL_LANG'" >&2
+      return 3;;
+  esac
+  MP_CODE="$(<<<"$MP_CODE" tr '\n' '\r' | sed -re 's~(^|\r)\s+~ ~g')"
+  MP_CODE="${MP_CODE# }"
+  MP_CODE="${MP_CODE% }"
   mpf_verify_tty_usage idle || return $?
   echo "D: upload $SRC_FN -> $DEST_ABS"
+  <<<';' mpf_communicate raw duplex || return $?
+  sleep 0.5s
   <<<"$MP_CODE" mpf_communicate raw duplex || return $?
   DEST_FN=
 }
